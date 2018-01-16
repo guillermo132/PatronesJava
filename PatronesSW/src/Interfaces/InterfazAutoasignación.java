707209389fc863b116eedcbae0456aea163767fb
José Ramón Casas Álvarez <joseramoncasas1a@gmail.com>
2018-01-16 12:02:02 +0100

    eliminado boton ver tfg

--- PatronesSW/src/Interfaces/InterfazAutoasignación.java
+++ PatronesSW/src/Interfaces/InterfazAutoasignación.java
@@ -118,7 +118,6 @@ public class InterfazAutoasignación extends javax.swing.JFrame {
         jLabel2 = new javax.swing.JLabel();
         jLabel3 = new javax.swing.JLabel();
         jButton1 = new javax.swing.JButton();
-        jButton2 = new javax.swing.JButton();
 
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
 
@@ -134,13 +133,6 @@ public class InterfazAutoasignación extends javax.swing.JFrame {
         jButton1.addActionListener(new java.awt.event.ActionListener() {
             public void actionPerformed(java.awt.event.ActionEvent evt) {
                 jButton1ActionPerformed(evt);
-            }
-        });
-
-        jButton2.setText("Ver detalles TFG");
-        jButton2.addActionListener(new java.awt.event.ActionListener() {
-            public void actionPerformed(java.awt.event.ActionEvent evt) {
-                jButton2ActionPerformed(evt);
             }
         });
 
@@ -161,12 +153,10 @@ public class InterfazAutoasignación extends javax.swing.JFrame {
                         .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
                         .addComponent(jLabel3)
                         .addGap(68, 68, 68))))
-            .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, layout.createSequentialGroup()
-                .addGap(110, 110, 110)
-                .addComponent(jButton2)
-                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+            .addGroup(layout.createSequentialGroup()
+                .addGap(195, 195, 195)
                 .addComponent(jButton1)
-                .addGap(97, 97, 97))
+                .addGap(0, 0, Short.MAX_VALUE))
         );
         layout.setVerticalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
@@ -179,9 +169,7 @@ public class InterfazAutoasignación extends javax.swing.JFrame {
                 .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
                 .addComponent(jScrollPane1, javax.swing.GroupLayout.PREFERRED_SIZE, 240, javax.swing.GroupLayout.PREFERRED_SIZE)
                 .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
-                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
-                    .addComponent(jButton1)
-                    .addComponent(jButton2))
+                .addComponent(jButton1)
                 .addContainerGap(16, Short.MAX_VALUE))
         );
 
@@ -194,32 +182,6 @@ public class InterfazAutoasignación extends javax.swing.JFrame {
         // TODO add your handling code here:
         dispose();
     }//GEN-LAST:event_jButton1ActionPerformed
-
-    private void jButton2ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton2ActionPerformed
-        // TODO add your handling code here:
-         try{
-            String key1 = jListInfo.getSelectedValue();
-            Tfg tfgSeleccionado = null;
-                for(int x=0;x<listaTFG.size();x++) {
-                    if(listaTFG.get(x).getTitulo().equals(key1)){
-                        tfgSeleccionado = listaTFG.get(x);
-                    }
-                }
-            InterfazVisualizarTfg lt = new InterfazVisualizarTfg(this.c, this.serializa, tfgSeleccionado){
-                @Override
-                public void dispose(){
-                    //Hacemos visible la principal
-                    getFrame().setVisible(true);
-                    //Cerramos
-                    super.dispose();
-                }
-            };
-            this.setVisible(false);
-            lt.setVisible(true);
-        }catch(Exception e){
-            JOptionPane.showMessageDialog(this,"No hay TFG seleccionado."); 
-        }
-    }//GEN-LAST:event_jButton2ActionPerformed
 
     /**
      * @param args the command line arguments
@@ -258,7 +220,6 @@ public class InterfazAutoasignación extends javax.swing.JFrame {
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
     private javax.swing.JButton jButton1;
-    private javax.swing.JButton jButton2;
     private javax.swing.JLabel jLabel1;
     private javax.swing.JLabel jLabel2;
     private javax.swing.JLabel jLabel3;
